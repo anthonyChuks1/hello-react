dabbacba117e20a615de0545f30c51024524bea7
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-10-09 06:56:45 -0400

    Implemented functionality for the application in index

--- src/index.js
+++ src/index.js
@@ -2,23 +2,30 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 const Button = (props) => {
   // your code here
+  return <button className = "button" onClick={props.onClick}>{props.children}</button>
 };
 
 const Application = () => {
-
+  const [name, setName] = useState('');
   // your code here
 
   const reset = () => {
     console.log("reset");
+    setName('');
     // your code here
   };
 
   return (
     <main>
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input placeholder = 'TYPE YOUR NAME' value = {name} onChange = {(e) => setName(e.target.value)}/>
+      <Button onClick = {reset}>RESET</Button>
+      
+      <h1>{name ? `Hello ${name}` : ''}</h1>
+      
     </main>
   );
 };
